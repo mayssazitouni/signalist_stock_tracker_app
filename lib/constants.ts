aa899eb60ab75100524017ca0b5a3d080b0dcd2e
mayssazitouni <mayssazitouni21@gmail.com>
2026-03-07 17:10:28 +0100

    implement header

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const NAV_ITEMS: {href:string; title:string}[] = [
+    { href: '/', label: 'Dashboard' },
+    { href: '/search', label: 'search' },
+    { href: '/watchlist', label: 'watchlist' },
+
+
+
+]
